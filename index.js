ce12659003048d002b7d30309aa29bd34d569f33
Naimur Rahman <naimur2231@gmail.com>
2023-05-02 14:09:11 +0600

    create api

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json')
+
 app.get('/', (req, res) => {
     res.send('royal meals server is running!!')
+  })
+
+  app.get('/chefs', (req, res) => {
+    res.send(chefs);
+  })
+
+  app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    const selectedChefs = chefs.find(n => parseInt(n.id) === id);
+    res.send(selectedChefs);
   })
 
   app.listen(port, () => {
